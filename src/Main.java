acda94b47196a173ce1c265a3164356f1d732e7a
INeedGitHub <takeshi.musgrave@mail.mcgill.ca>
2013-11-02 01:48:04 -0400

    Wrote some comments in the main class so you can see how the Map class might work. Also, the Builder and GarbageCollector classes seem to be unnecessary. We can use Competitor by itself.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
-
 public class Main {
+
+	public static void main(String args[]) {
+
+		// Get bluetooth info
+
+		// Map.setRedZone(new Coordinates(x1,y1), new Coordinates(x2,y2))
+		// Map.setGreenZone(new Coordinates(x3,y3), new Coordinates(x4,y4))
+
+		// If we are the garbage collector, then Map.setTargetZone(Map.redZone),
+		// else Map.setTargetZone(Map.greenZone)
+		
+		// Competitor.play()
+
+	}
 
 }

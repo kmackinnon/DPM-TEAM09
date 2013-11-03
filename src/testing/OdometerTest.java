7de5643e2972e26e65224f3a1c482ddea9923385
Sidney Ng <yendisng@gmail.com>
2013-11-03 15:11:13 -0500

    Added test for Odometer with 2 test cases. First drive in a square similar to lab 2. Second to drive in pattern similar to lab 4.

---
+++ src/testing/OdometerTest.java
@@ -0,0 +1,57 @@
+package testing;
+
+import lejos.nxt.Button;
+import robot.MobileRobot;
+
+/**
+ * Test functionality of the Mobile Robot and accuracy of the Odometer.
+ * 1. Drive Robot in a square - measure x and y from actual position and theta from actual heading
+ * 	  Use the travelMag methods
+ * 2. Do the same as (1) but use the travelCoordinate method and travel in a triangle
+ * 
+ * @author Sidney Ng
+ *
+ */
+
+public class OdometerTest {
+	
+	public static void main(String[] args) {
+		MobileRobot robot = new MobileRobot();
+
+		int option = 0; // don't bother with button inputs: just change this variable
+		
+		while (option == 0)
+			option = Button.waitForAnyPress();
+		
+		switch(option) {
+		case Button.ID_LEFT:
+			squareTest(robot);
+			break;
+		case Button.ID_RIGHT:
+			triangleTest(robot);
+			break;
+		default:
+			System.out.println("Error - invalid button");
+			System.exit(-1);
+			break;
+		}
+		
+		Button.waitForAnyPress();
+		System.exit(0);
+	}
+	
+	public static void squareTest(MobileRobot robot) {
+		for (int i = 0; i < 4; i++) {
+		robot.travelMag(60.96);
+			robot.turnTo(MobileRobot.odo.getAng() + 90.0);
+		}
+		robot.turnTo(0.0);
+	}
+	
+	public static void triangleTest(MobileRobot robot) {
+		robot.travelCoordinate(60.0, 30.0);
+		robot.travelCoordinate(30.0, 30.0);
+		robot.travelCoordinate(30.0, 60.0);
+		robot.travelCoordinate(60.0, 0.0);
+	}
+}

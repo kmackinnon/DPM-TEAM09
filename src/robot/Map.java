83feaff0fc9802dc5db95969d4689f4867157f22
INeedGitHub <takeshi.musgrave@mail.mcgill.ca>
2013-11-03 23:29:20 -0500

    More javadoc

--- src/robot/Map.java
+++ src/robot/Map.java
@@ -1,4 +1,5 @@
 package robot;
+
 /**
  * Map contains a representation of the game area. This includes the locations
  * of obstacles, the red and green zones, and the 4 corners
@@ -8,66 +9,130 @@ import java.util.ArrayList;
 
 public class Map {
 
-	public static ArrayList<Coordinates> obstacles;
+	private static ArrayList<Coordinates> obstacles;
 
-	public static double topBoundary;
-	public static double bottomBoundary;
-	public static double leftBoundary;
-	public static double rightBoundary;
+	private static double topBoundary;
+	private static double bottomBoundary;
+	private static double leftBoundary;
+	private static double rightBoundary;
 
 	public static SquareRegion redZone;
 	public static SquareRegion greenZone;
+
+	/**
+	 * The zone that the robot moves to when carrying a styrofoam block. This is
+	 * equal to greenZone when in Builder mode, and redZone when in
+	 * GarbageCollector mode
+	 */
 	public static SquareRegion targetZone;
 
+	/**
+	 * The zone that the robot is not allowed to enter. This is equal to redZone
+	 * when in Builder mode, and greenZone when in GarbageCollector mode
+	 */
+	public static SquareRegion forbiddenZone;
+
+	/** This is the bottom left starting tile of the board */
 	public static SquareRegion cornerX1;
+
+	/** This is the bottom right starting tile of the board */
 	public static SquareRegion cornerX2;
+
+	/** This is the top right starting tile of the board */
 	public static SquareRegion cornerX3;
+
+	/** This is the top left starting tile of the board */
 	public static SquareRegion cornerX4;
 
-	
-	public static void addObstacle(double x, double y){
-		
-	}
-	
-	
-	public static void setTargetZone(SquareRegion target) {
-		targetZone = target;
+	/**
+	 * Records the coordinates of an obstacle.
+	 * 
+	 * @param x
+	 *            The x coordinate of the obstacle.
+	 * @param y
+	 *            The y coordinate of the obstacle.
+	 */
+	public static void addObstacle(double x, double y) {
+
 	}
 
+	/**
+	 * Sets the targetZone and forbiddenZone of the robot. This is determined at
+	 * the very beginning of the game.
+	 * 
+	 * @param target
+	 *            The target zone. Green if Builder, and Red if Garbage
+	 *            Collector
+	 */
+	public static void setTargetZone(SquareRegion target) {
+		targetZone = target;
+
+		if (targetZone == redZone) {
+			forbiddenZone = greenZone;
+		}
+
+		else {
+			forbiddenZone = redZone;
+		}
+
+	}
+
+	/**
+	 * Creates the redZone rectangle.
+	 * 
+	 * @param topRightCorner
+	 *            the coordinates of the top right corner of the redZone.
+	 * @param bottomLeftCorner
+	 *            the coordinates of the bottom left corner of the redZone.
+	 */
 	public static void setRedZone(Coordinates topRightCorner,
 			Coordinates bottomLeftCorner) {
 
 		redZone = new SquareRegion(topRightCorner, bottomLeftCorner);
 	}
 
+	/**
+	 * Creates the greenZone rectangle.
+	 * 
+	 * @param topRightCorner
+	 *            the coordinates of the top right corner of the greenZone.
+	 * @param bottomLeftCorner
+	 *            the coordinates of the bottom left corner of the greenZone.
+	 */
 	public static void setGreenZone(Coordinates topRightCorner,
 			Coordinates bottomLeftCorner) {
 		greenZone = new SquareRegion(topRightCorner, bottomLeftCorner);
 	}
 
-	public boolean isNearForbiddenRegion(double x, double y) {
+	/**
+	 * Returns a boolean indicating if the robot is near a forbidden region.
+	 * 
+	 * @param x
+	 *            The x position of the robot.
+	 * @param y
+	 *            The y position of the robot.
+	 * @return true if the robot is close to a forbidden region, false
+	 *         otherwise.
+	 */
+	public boolean isCloseToForbiddenRegion(double x, double y) {
 
-		if (redZone.isNear(x, y)) {
+		if (forbiddenZone.isClose(x, y)) {
 			return true;
 		}
 
-		else if (greenZone.isNear(x, y)) {
+		else if (cornerX1.isClose(x, y)) {
 			return true;
 		}
 
-		else if (cornerX1.isNear(x, y)) {
+		else if (cornerX2.isClose(x, y)) {
 			return true;
 		}
 
-		else if (cornerX2.isNear(x, y)) {
+		else if (cornerX3.isClose(x, y)) {
 			return true;
 		}
 
-		else if (cornerX3.isNear(x, y)) {
-			return true;
-		}
-
-		else if (cornerX4.isNear(x, y)) {
+		else if (cornerX4.isClose(x, y)) {
 			return true;
 		}
 
@@ -77,7 +142,18 @@ public class Map {
 	}
 
 	// TODO fill in
-	public ArrayList<Coordinates> findBestPath(Coordinates current, Coordinates target){
+	/**
+	 * Returns a list of waypoints of the shortest path to a destination.
+	 * 
+	 * @param current
+	 *            the current coordinates of the robot
+	 * @param destination
+	 *            the coordinates of the destination
+	 * @return an arraylist of coordinates that represent the shortest path from
+	 *         the current position to the destination
+	 */
+	public ArrayList<Coordinates> findBestPath(Coordinates current,
+			Coordinates destination) {
 
 		return new ArrayList<Coordinates>();
 	}
--- src/robot/SquareRegion.java
+++ src/robot/SquareRegion.java
@@ -1,31 +1,61 @@
 package robot;
 
+/**
+ * SquareRegion is a class meant to represent rectangular areas on the board.
+ * This is simply a way to hide some code that is needed to determine if the
+ * robot is entering a "forbidden" zone. It may also make it easier to write the
+ * code for finding optimal paths
+ * 
+ */
+
 public class SquareRegion {
-	
+
 	private Coordinates topRightCorner;
 	private Coordinates bottomLeftCorner;
-	
-	public SquareRegion(Coordinates c1, Coordinates c2){
+	private final double closeDistance = 6;
+
+	/**
+	 * 
+	 * @param c1
+	 *            The coordinates of the top right corner of the rectangle.
+	 * @param c2
+	 *            The coordinates of the bottom left corner of the rectangle.
+	 */
+	public SquareRegion(Coordinates c1, Coordinates c2) {
 		this.topRightCorner = c1;
-		this.bottomLeftCorner = c2; 
+		this.bottomLeftCorner = c2;
 	}
-	
-	//TODO fill in
-	public boolean isNear(double x, double y){
-		
+
+	// TODO fill in
+	/**
+	 * Returns a boolean indicating if the robot is close to a rectangle.
+	 * 
+	 * @param x
+	 *            The current x position of the robot.
+	 * @param y
+	 *            The current y position of the robot.
+	 * @return true if the robot is close to the rectangle, false otherwise.
+	 *         "Close" is defined in the variable closeDistance.
+	 */
+	public boolean isClose(double x, double y) {
+
 		return false;
 	}
-	
-	//TODO fill in
-	public double getDistance(double x, double y){
-		
-		
+
+	// TODO fill in
+	/**
+	 * Returns a double indicating the distance to the closest point of the
+	 * rectangle.
+	 * 
+	 * @param x
+	 *            The current x position of the robot.
+	 * @param y
+	 *            The current y position of the robot.
+	 * @return the distance to the closest point of the rectangle
+	 */
+	public double getDistance(double x, double y) {
+
 		return 0;
 	}
-	
-	
-	
-	
-	
 
 }

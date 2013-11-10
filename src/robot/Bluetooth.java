0a405ea6eb0c07cf6117e5d30a61d8b1ba96b626
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-09 20:10:22 -0500

    Adding the supplied Bluetooth connection code to the project.

--- src/robot/Bluetooth.java
+++ src/robot/Bluetooth.java
@@ -1,5 +1,12 @@
 package robot;
 
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import bluetooth.BluetoothConnection;
+import bluetooth.PlayerRole;
+import bluetooth.StartCorner;
+import bluetooth.Transmission;
+
 /**
  * 
  * This class will be the controller between our robot and the input data. Our
@@ -11,4 +18,33 @@ package robot;
  */
 public class Bluetooth {
 
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		BluetoothConnection conn = new BluetoothConnection();
+
+		// as of this point, the bluetooth connection is closed again, and you
+		// can pair to another NXT (or PC) if you wish
+
+		// example usage of Transmission class
+		Transmission t = conn.getTransmission();
+		
+		if (t == null) {
+			LCD.drawString("Failed to read transmission", 0, 5);
+		} else {
+			StartCorner corner = t.startingCorner;
+			PlayerRole role = t.role;
+
+			// green zone is defined by (bottom-left and top-right) corners:
+			int[] greenZone = t.greenZone;
+
+			// red zone is defined by these (bottom-left and top-right) corners:
+			int[] redZone = t.redZone;
+
+			// print out the transmission information to the LCD
+			conn.printTransmission();
+		}
+
+		// stall until user decides to end program
+		Button.waitForAnyPress();
+	}
 }

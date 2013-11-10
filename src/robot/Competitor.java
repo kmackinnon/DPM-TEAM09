a6dc6c330d996607479e1e62c8bbb5c167ddc294
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-09 20:34:34 -0500

    Merged Bluetooth class into Competitor. Realized that Bluetooth was unnecessary and deleted it.

--- src/robot/Competitor.java
+++ src/robot/Competitor.java
@@ -1,5 +1,12 @@
 package robot;
 
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import bluetooth.BluetoothConnection;
+import bluetooth.PlayerRole;
+import bluetooth.StartCorner;
+import bluetooth.Transmission;
+
 /**
  * This class contains the main function, and gives responsibility of the robot
  * to Localizer, Explorer, or BlockMover, depending on the situation. It
@@ -12,22 +19,47 @@ package robot;
 
 public class Competitor {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) throws InterruptedException {
 
-		//GetBluetooth info
-		
+		BluetoothConnection conn = new BluetoothConnection();
+
+		// as of this point, the bluetooth connection is closed again, and you
+		// can pair to another NXT (or PC) if you wish
+
+		// example usage of Transmission class
+		Transmission t = conn.getTransmission();
+
+		if (t == null) {
+			LCD.drawString("Failed to read transmission", 0, 5);
+		} else {
+			StartCorner corner = t.startingCorner;
+			PlayerRole role = t.role;
+
+			// green zone is defined by (bottom-left and top-right) corners:
+			int[] greenZone = t.greenZone;
+
+			// red zone is defined by these (bottom-left and top-right) corners:
+			int[] redZone = t.redZone;
+
+			// print out the transmission information to the LCD
+			conn.printTransmission();
+		}
+
+		// stall until user decides to end program
+		Button.waitForAnyPress();
+
 		Map.initializeMap();
-		
-		//Map.setForbiddenZone;
-		//Map.setTargetZone;
-		
-		//if we are builder
-		//SensorMotorUser.isBuilder(true);
-		
-		//if we are garbage collector
-		//SensorMotorUser.isBuilder(false);
-		
-		
+
+		// Map.setForbiddenZone;
+		// Map.setTargetZone;
+
+		// if we are builder
+		// SensorMotorUser.isBuilder(true);
+
+		// if we are garbage collector
+		// SensorMotorUser.isBuilder(false);
+
 		Localizer localizer = new Localizer();
 		Explorer explorer = new Explorer();
 		BlockMover blockMover = new BlockMover();
@@ -37,6 +69,5 @@ public class Competitor {
 		explorer.lookForStyrofoamBlocks();
 
 	}
-
 
 }

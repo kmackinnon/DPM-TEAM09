1f583bb7b750f1381d3a7a6fa412c554ab413924
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-14 11:56:15 -0500

    Added the ability to run the program in Bluetooth (Demo) mode or Test mode. The robot's starting corner gets set either way and then localization occurs. There is something wrong with setting forbidden and target zones. The robot shows error 67 when attempting to set those zones manually.

--- src/robot/Competitor.java
+++ src/robot/Competitor.java
@@ -3,8 +3,10 @@ package robot;
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.Sound;
+import bluetooth.BluetoothConnection;
 import bluetooth.PlayerRole;
 import bluetooth.StartCorner;
+import bluetooth.Transmission;
 
 /**
  * This class contains the main function, and gives responsibility of the robot
@@ -23,76 +25,104 @@ public class Competitor {
 	private static int[] greenZone;
 	private static int[] redZone;
 
-	@SuppressWarnings("unused")
 	public static void main(String[] args) throws InterruptedException {
 
-		/*
-		 * BluetoothConnection conn = new BluetoothConnection();
-		 * 
-		 * 
-		 * 
-		 * // as of this point, the bluetooth connection is closed again, and
-		 * you // can pair to another NXT (or PC) if you wish
-		 * 
-		 * // example usage of Transmission class Transmission t =
-		 * conn.getTransmission();
-		 * 
-		 * if (t == null) { LCD.drawString("Failed to read transmission", 0, 5);
-		 * } else { corner = t.startingCorner; role = t.role;
-		 * 
-		 * // green zone is defined by (bottom-left and top-right) corners:
-		 * greenZone = t.greenZone;
-		 * 
-		 * // red zone is defined by these (bottom-left and top-right) corners:
-		 * redZone = t.redZone;
-		 * 
-		 * // print out the transmission information to the LCD
-		 * conn.printTransmission(); }
-		 * 
-		 * 
-		 * if(role.getId()==1){ SensorMotorUser.becomeBuilder(true); }
-		 * 
-		 * else if(role.getId()==2){ SensorMotorUser.becomeBuilder(false); }
-		 */
-
-		/*
-		 * if(SensorMotorUser.isBuilder()){ Map.setTargetZone(greenZone);
-		 * Map.setForbiddenZone(redZone); }
-		 * 
-		 * else if(!SensorMotorUser.isBuilder()){ Map.setTargetZone(redZone);
-		 * Map.setForbiddenZone(greenZone); }
-		 */
-
-		Button.waitForAnyPress();
-
-		/*
-		 * int[] test1 = {0,5,2,9};
-		 * 
-		 * int[] test2 = {6,3,8,4};
-		 */
-
 		Map.initializeMap();
-
-		/*
-		 * Map.setForbiddenZone(test1);
-		 * 
-		 * Map.setTargetZone(test2);
-		 */
-
-		SensorMotorUser.setStartCorner(new int[] { 0, 0 });
 
 		Localizer localizer = new Localizer();
 		Explorer explorer = new Explorer();
 		// BlockMover blockMover = new BlockMover();
 
-		localizer.localize();
+		int buttonChoice;
 
+		// Left or Right buttons must be pressed for the robot to begin
+		do {
+			LCD.clear(); // clear the display
+
+			// chooses BT for competition or fixed values for testing
+			LCD.drawString("< Left  | Right >", 0, 0);
+			LCD.drawString("        |        ", 0, 1);
+			LCD.drawString("< Demo  | Test > ", 0, 2);
+
+			buttonChoice = Button.waitForAnyPress();
+
+		} while (buttonChoice != Button.ID_LEFT
+				&& buttonChoice != Button.ID_RIGHT);
+
+		// Lets user choose whether to get starting data from BT or test data
+		if (buttonChoice == Button.ID_LEFT) {
+			connectBT(); // start the BT connection determines characteristics
+
+		} else {
+			int[] testGreen = { 0, 5, 2, 9 }; // green zone
+			int[] testRed = { 6, 3, 8, 4 }; // red zone
+
+			// set corner
+			corner = StartCorner.BOTTOM_LEFT;
+
+			// set role. For now, assume the robot will be a builder.
+			SensorMotorUser.becomeBuilder(true);
+
+			// TODO fix Map. Something there is causing exception 67.
+			/*Map.setForbiddenZone(testGreen);
+			Map.setTargetZone(testRed);*/
+		}
+
+		// sets the start corner from the test data or from the BT connection
+		SensorMotorUser
+				.setStartCorner(new int[] { corner.getX(), corner.getY() });
+		
+	
+		localizer.localize();
+		Sound.beep(); 
+		
 		explorer.lookForStyrofoamBlocks();
 
-		Button.waitForAnyPress();
-
 		System.exit(0);
-
 	}
 
+	/** This sets up the demo by setting starting corner and player type. */
+	public static void connectBT() {
+		BluetoothConnection conn = new BluetoothConnection();
+
+		// as of this point, the bluetooth connection is closed again, and
+		// you can pair to another NXT (or PC) if you wish
+
+		// example usage of Transmission class
+		Transmission t = conn.getTransmission();
+
+		if (t == null) {
+			LCD.drawString("Failed to read transmission", 0, 5);
+		} else {
+			corner = t.startingCorner;
+			role = t.role;
+		}
+
+		// green zone is defined by (bottom-left and top-right) corners:
+		greenZone = t.greenZone;
+
+		// red zone is defined by these (bottom-left and top-right) corners:
+		redZone = t.redZone;
+
+		// print out the transmission information to the LCD
+		conn.printTransmission();
+
+		if (role.getId() == 1) {
+			SensorMotorUser.becomeBuilder(true);
+		}
+
+		else if (role.getId() == 2) {
+			SensorMotorUser.becomeBuilder(false);
+		}
+
+		if (SensorMotorUser.isBuilder()) {
+			Map.setTargetZone(greenZone);
+			Map.setForbiddenZone(redZone);
+		}
+
+		else if (!SensorMotorUser.isBuilder()) {
+			Map.setTargetZone(redZone);
+			Map.setForbiddenZone(greenZone);
+		}
+	}
 }

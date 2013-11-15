fd97a9df01619e0463114308be8d10ca7379749d
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-14 22:10:40 -0500

    Adjusted the method call to get the Intersection from the odometer positions after changes in the Map and Intersection classes.

--- src/robot/MobileRobot.java
+++ src/robot/MobileRobot.java
@@ -2,21 +2,22 @@ package robot;
 
 import java.util.ArrayList;
 
-
-/**MobileRobot contains all the methods needed for the robot to move to a location, and to turn to an angle. 
- * This includes traveling to a point while navigating around obstacles.  
+/**
+ * MobileRobot contains all the methods needed for the robot to move to a
+ * location, and to turn to an angle. This includes traveling to a point while
+ * navigating around obstacles.
  * 
  * @author Simon Lee, Sidney Ng
- *
+ * 
  */
 
-public class MobileRobot extends SensorMotorUser{
+public class MobileRobot extends SensorMotorUser {
 	public static Odometer odo = new Odometer(true);
 	public static BlockDetector blockDetector;
-	
-	double [] pos = new double [3];
+
+	double[] pos = new double[3];
 	private double forwardSpeed, rotationSpeed;
-	
+
 	/**
 	 * Default Constructor
 	 * <p>
@@ -24,53 +25,49 @@ public class MobileRobot extends SensorMotorUser{
 	 */
 	public MobileRobot() {
 	}
-	
-	
-	public void travelTo(Intersection destination){
-		
-		Intersection source = Intersection.convertToIntersection(odo.getX(), odo.getY());
-		
-		ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithm(source,destination);
-		
-		for(Intersection intersection : listOfWayPoints){
-			
+
+	public void travelTo(Intersection destination) {
+
+		Intersection source = Map.getIntersection(odo.getX(), odo.getY());
+
+		ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithm(source,
+				destination);
+
+		for (Intersection intersection : listOfWayPoints) {
+
 			travelCoordinate(intersection.getXInCm(), intersection.getYInCm());
-			
+
 		}
-			
+
 	}
-	
-	
-	public void travelToNeighbor(Intersection destination){
-		
+
+	public void travelToNeighbor(Intersection destination) {
 		travelCoordinate(destination.getXInCm(), destination.getYInCm());
-		
 	}
-	
-	public void travelToTargetZone(){
-		
-		Intersection source = Intersection.convertToIntersection(odo.getX(), odo.getY());
-		
-		ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithmForTargetZone(source);
-		
-		for(Intersection intersection : listOfWayPoints){
-			
+
+	public void travelToTargetZone() {
+
+		Intersection source = Map.getIntersection(odo.getX(), odo.getY());
+
+		ArrayList<Intersection> listOfWayPoints = Dijkstra
+				.algorithmForTargetZone(source);
+
+		for (Intersection intersection : listOfWayPoints) {
+
 			travelCoordinate(intersection.getXInCm(), intersection.getYInCm());
-			
+
 		}
-		
+
 	}
-	
-	
-	
-	
+
 	/**
 	 * Robot travels a certain distance
 	 * 
-	 * @param distance distance to travel
+	 * @param distance
+	 *            distance to travel
 	 */
-	public void travelMag(double distance){
-		double[] initPos = new double [3], currPos = new double [3];
+	public void travelMag(double distance) {
+		double[] initPos = new double[3], currPos = new double[3];
 		setRotationSpeed(0.0);
 		odo.getPosition(initPos);
 		odo.getPosition(currPos);
@@ -78,103 +75,120 @@ public class MobileRobot extends SensorMotorUser{
 			setForwardSpeed(-FORWARD_SPEED);
 		else
 			setForwardSpeed(FORWARD_SPEED);
-		while (Math.pow((currPos[0]-initPos[0]), 2) + Math.pow((currPos[1]-initPos[1]), 2) < distance * distance) {//pythagoros to determine how far left to go
+		// pythagoras to determine how far left to go
+		while (Math.pow((currPos[0] - initPos[0]), 2)
+				+ Math.pow((currPos[1] - initPos[1]), 2) < distance * distance) {
 			odo.getPosition(currPos);
 		}
 		setForwardSpeed(0.0);
 	}
-	
-	public void travelCoordinate(int x, int y){
-		
-		double xInCm = x*Map.TILE_SIZE;
-		double yInCm = y*Map.TILE_SIZE;
-		
-		travelCoordinate(xInCm,yInCm);
-		
+
+	public void travelCoordinate(int x, int y) {
+
+		double xInCm = x * Map.TILE_SIZE;
+		double yInCm = y * Map.TILE_SIZE;
+
+		travelCoordinate(xInCm, yInCm);
+
 	}
-	
-	
+
 	/**
 	 * Move robot to position at (x,y)
 	 * <p>
 	 * Will orient robot to face coordinate before moving in a straight line
 	 * 
-	 * @param x x coordinate to move to
-	 * @param y y coordinate to move to
+	 * @param x
+	 *            x coordinate to move to
+	 * @param y
+	 *            y coordinate to move to
 	 */
-	public void travelCoordinate(double x, double y){
+	public void travelCoordinate(double x, double y) {
 		odo.getPosition(pos);
-		while (Math.sqrt(Math.pow((x-pos[0]), 2) + Math.pow((y-pos[1]), 2)) > 3){	
+		while (Math.sqrt(Math.pow((x - pos[0]), 2) + Math.pow((y - pos[1]), 2)) > 3) {
 			odo.getPosition(pos);
-	
-				if(Math.abs(y - pos[1]) < 2){//if you only need to move horizontally
-					if(Math.abs(x -pos[0])>2){
-						if(pos[0]>x) turnTo(-90);//position is to left
-						else turnTo(90);
-						}
-					}
-				//position is to right
-				else{
-					if(y>pos[1])
-					turnTo(Math.toDegrees(Math.atan((x-pos[0])/(y-pos[1]))));	
-					else{
-						if(Math.abs(x -pos[0])>2){
-							if(pos[0]>x)
-							turnTo(-1*Math.toDegrees(Math.atan((y-pos[1])/(x-pos[0]))) - 90);//counter clockwise
-							else
-							turnTo(-1*Math.toDegrees(Math.atan((y-pos[1])/(x-pos[0]))) + 90);//clockwise
-						}
+
+			if (Math.abs(y - pos[1]) < 2) {// if you only need to move
+											// horizontally
+				if (Math.abs(x - pos[0]) > 2) {
+					if (pos[0] > x)
+						turnTo(-90);// position is to left
+					else
+						turnTo(90);
+				}
+			}
+			// position is to right
+			else {
+				if (y > pos[1])
+					turnTo(Math.toDegrees(Math
+							.atan((x - pos[0]) / (y - pos[1]))));
+				else {
+					if (Math.abs(x - pos[0]) > 2) {
+						if (pos[0] > x)
+							turnTo(-1
+									* Math.toDegrees(Math.atan((y - pos[1])
+											/ (x - pos[0]))) - 90);// counter
+																	// clockwise
+						else
+							turnTo(-1
+									* Math.toDegrees(Math.atan((y - pos[1])
+											/ (x - pos[0]))) + 90);// clockwise
 					}
 				}
+			}
 
-				
-				setRotationSpeed(0.0);
-				
-				travelMag(Math.sqrt(Math.pow((x-pos[0]), 2) + Math.pow((y-pos[1]), 2)));//after orientation travel there
+			setRotationSpeed(0.0);
+
+			travelMag(Math.sqrt(Math.pow((x - pos[0]), 2)
+					+ Math.pow((y - pos[1]), 2)));// after orientation travel
+													// there
 		}
 		setSpeeds(0.0, 0.0);
 	}
-	
+
 	/**
 	 * Turns robot by specified value.
 	 * <p>
-	 * Negative angle values turn robot counterclockwise. Positive angle values turn robot clockwise.
+	 * Negative angle values turn robot counterclockwise. Positive angle values
+	 * turn robot clockwise.
 	 * 
-	 * @param angle angle to turn
+	 * @param angle
+	 *            angle to turn
 	 */
 	public void turnTo(double angle) {
-		
-		double [] currPos = new double [3];
+
+		double[] currPos = new double[3];
 		double currSpeed = ROTATION_SPEED;
 		double angDiff;
-		
+
 		setForwardSpeed(0.0);
-		
+
 		odo.getPosition(currPos);
-		
-		angDiff = Odometer.minimumAngleFromTo(currPos[2], angle);//find minimum angle between current angle and where we need to go
-		
+
+		// find minimum angle between current angle and where to go
+		angDiff = Odometer.minimumAngleFromTo(currPos[2], angle);
+
 		if (angDiff > 0.0)
-			setRotationSpeed(currSpeed);//clockwise
+			setRotationSpeed(currSpeed);// clockwise
 		else
-			setRotationSpeed(currSpeed *= -1);//counterclockwise
-		
-		
-		while (Math.abs(angDiff) > 0.5) {//move to angle
+			setRotationSpeed(currSpeed *= -1);// counterclockwise
+
+		while (Math.abs(angDiff) > 0.5) {// move to angle
 			if (currSpeed > 0.0 && angDiff < 0.0)
 				setRotationSpeed(currSpeed *= -0.5);
 			else if (currSpeed < 0.0 && angDiff > 0.0)
 				setRotationSpeed(currSpeed *= -0.5);
-			
+
 			odo.getPosition(currPos);
 			angDiff = Odometer.minimumAngleFromTo(currPos[2], angle);
 		}
 		setSpeeds(0.0, 0.0);
 	}
-	
+
 	// mutators
 	/**
-	 * Sets linear speed. Calls setSpeeds(double forwardSpeed, double rotationalSpeed) with rotationalSpeed equal to 0
+	 * Sets linear speed. Calls setSpeeds(double forwardSpeed, double
+	 * rotationalSpeed) with rotationalSpeed equal to 0
+	 * 
 	 * @param speed
 	 */
 	public void setForwardSpeed(double speed) {
@@ -182,38 +196,44 @@ public class MobileRobot extends SensorMotorUser{
 		rotationSpeed = 0;
 		setSpeeds(forwardSpeed, rotationSpeed);
 	}
-	
+
 	/**
-	 * Sets rotational speed. Calls setSpeeds(double forwardSpeed, double rotationalSpeed) with forwardSpeed equal to 0
+	 * Sets rotational speed. Calls setSpeeds(double forwardSpeed, double
+	 * rotationalSpeed) with forwardSpeed equal to 0
 	 * 
-	 * @param speed rotational speed to set motors
+	 * @param speed
+	 *            rotational speed to set motors
 	 */
 	public void setRotationSpeed(double speed) {
 		forwardSpeed = 0;
 		rotationSpeed = speed;
 		setSpeeds(forwardSpeed, rotationSpeed);
 	}
-	
+
 	/**
 	 * Sets forward and rotational speed.
 	 * <p>
-	 * Positive rotational speed designated in clockwise direction. Allows for traveling in an arc
+	 * Positive rotational speed designated in clockwise direction. Allows for
+	 * traveling in an arc
 	 * <p>
-	 * For speeds of zero, the speed is actually set to 1. Setting motor speed to 0 does not allow for further increases to speed.
+	 * For speeds of zero, the speed is actually set to 1. Setting motor speed
+	 * to 0 does not allow for further increases to speed.
 	 * 
-	 * @param forwardSpeed the forward speed to set the motors
-	 * @param rotationalSpeed the rotational speed to set the motors
+	 * @param forwardSpeed
+	 *            the forward speed to set the motors
+	 * @param rotationalSpeed
+	 *            the rotational speed to set the motors
 	 */
-	public void setSpeeds(double forwardSpeed, double rotationalSpeed) {//clockwise
+	public void setSpeeds(double forwardSpeed, double rotationalSpeed) {// clockwise
 		double leftSpeed, rightSpeed;
 
 		this.forwardSpeed = forwardSpeed;
-		this.rotationSpeed = rotationalSpeed; 
+		this.rotationSpeed = rotationalSpeed;
 
-		leftSpeed = (forwardSpeed + rotationalSpeed * width * Math.PI / 360.0) *
-				180.0 / (leftRadius * Math.PI);
-		rightSpeed = (forwardSpeed - rotationalSpeed * width * Math.PI / 360.0) *
-				180.0 / (rightRadius * Math.PI);
+		leftSpeed = (forwardSpeed + rotationalSpeed * width * Math.PI / 360.0)
+				* 180.0 / (leftRadius * Math.PI);
+		rightSpeed = (forwardSpeed - rotationalSpeed * width * Math.PI / 360.0)
+				* 180.0 / (rightRadius * Math.PI);
 
 		// set motor directions
 		if (leftSpeed > 0.0)
@@ -222,34 +242,35 @@ public class MobileRobot extends SensorMotorUser{
 			leftMotor.backward();
 			leftSpeed = -leftSpeed;
 		}
-		
+
 		if (rightSpeed > 0.0)
 			rightMotor.forward();
 		else {
 			rightMotor.backward();
 			rightSpeed = -rightSpeed;
 		}
-		
+
 		// set motor speeds
 		if (leftSpeed > 900.0)
 			leftMotor.setSpeed(900);
-		if(leftSpeed == 0.0)
-			// If the speed is set to 0 then the Motors come to a complete stop and can't be restarted
-			// Setting speed to 1 virtually stops motor movement, but allows for further changes in motor speed
+		if (leftSpeed == 0.0)
+			// If the speed is set to 0 then the Motors come to a complete stop
+			// and can't be restarted
+			// Setting speed to 1 virtually stops motor movement, but allows for
+			// further changes in motor speed
 			leftMotor.setSpeed(1);
 		else
-			leftMotor.setSpeed((int)leftSpeed);
-		
+			leftMotor.setSpeed((int) leftSpeed);
+
 		if (rightSpeed > 900.0)
 			rightMotor.setSpeed(900);
-		else
-		if(rightSpeed == 0.0)
+		else if (rightSpeed == 0.0)
 			rightMotor.setSpeed(1);
 		else
-			rightMotor.setSpeed((int)rightSpeed);
-		
+			rightMotor.setSpeed((int) rightSpeed);
+
 	}
-	
+
 	/**
 	 * Stops both motors. Calls NXTRegulatedMotor.stop()
 	 */

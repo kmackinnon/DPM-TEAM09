49c1165e166ddc9b912eb87c8a8aecf501ddc2fc
TakeshiMusgrave <takeshi.musgrave@mail.mcgill.ca>
2013-11-20 00:12:20 -0500

    Dijkstra test has edges removed after the initial algorithm is done.

--- src/testing/DijkstraTest.java
+++ src/testing/DijkstraTest.java
@@ -1,58 +1,94 @@
 package testing;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import lejos.nxt.comm.RConsole;
 import robot.Dijkstra;
 import robot.Intersection;
 import robot.Map;
-import robot.SensorMotorUser;
 
 public class DijkstraTest {
 
 	public static void main(String[] args) {
 		int[] forbiddenZone = { 2, 4, 5, 6 };
-		int[] targetZone = {3,9,4,10};
-		
+		int[] targetZone = { 4, 9, 5, 10 };
+
 		RConsole.open();
 		Map.initializeMap();
-		
-	
 
 		Map.setForbiddenZone(forbiddenZone);
 		Map.setTargetZone(targetZone);
 		
+
+		
 		
 
-//		ArrayList<Intersection> testTargetZone = Map.getTargetZone();
-//		
-//		for(Intersection intersection : testTargetZone){
-//			
-//			if(intersection.isTarget()){
-//			
-//				RConsole.println(intersection.getX() + " " + intersection.getY());
-//			}
-//		}
-//		
-//		RConsole.print("\n");
-//		RConsole.print("\n");
-//		RConsole.print("\n");
-		
+		// ArrayList<Intersection> testTargetZone = Map.getTargetZone();
+		//
+		// for(Intersection intersection : testTargetZone){
+		//
+		// if(intersection.isTarget()){
+		//
+		// RConsole.println(intersection.getX() + " " + intersection.getY());
+		// }
+		// }
+		//
+		// RConsole.print("\n");
+		// RConsole.print("\n");
+		// RConsole.print("\n");
 
-		Intersection startingPoint = new Intersection(0, 0);
+		// If you put the starting point anywhere above the first row, then the
+		// list of way points will not print in the graphic
+		Intersection startingPoint = new Intersection(5, 0);
 
 		Intersection destination = new Intersection(10, 10);
 
-		//ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithm(
-			//	startingPoint, destination);
-				
-		ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithmForTargetZone(startingPoint);
+		// ArrayList<Intersection> listOfWayPoints = Dijkstra.algorithm(
+		// startingPoint, destination);
 
+		ArrayList<Intersection> listOfWayPoints = Dijkstra
+				.algorithmForTargetZone(startingPoint);
+
+		Map.removeEdge(new Intersection(5,3), new Intersection(6,4));
+		Map.removeEdge(new Intersection(6,3), new Intersection(6,4));
+		Map.removeEdge(new Intersection(7,3), new Intersection(6,4));
+		
+		Map.removeEdge(new Intersection(6,3), new Intersection(7,4));
+		Map.removeEdge(new Intersection(7,3), new Intersection(7,4));
+		Map.removeEdge(new Intersection(8,3), new Intersection(7,4));
+		
+		Map.removeEdge(new Intersection(7,3), new Intersection(8,4));
+		Map.removeEdge(new Intersection(8,3), new Intersection(8,4));
+		Map.removeEdge(new Intersection(9,3), new Intersection(8,4));
+		
+		Map.removeEdge(new Intersection(8,3), new Intersection(9,4));
+		Map.removeEdge(new Intersection(9,3), new Intersection(9,4));
+		Map.removeEdge(new Intersection(10,3), new Intersection(9,4));
+		
+		Map.removeEdge(new Intersection(9,3), new Intersection(10,4));
+		
 		for (int i = 0; i < listOfWayPoints.size(); i++) {
 			int x = listOfWayPoints.get(i).getX();
 			int y = listOfWayPoints.get(i).getY();
 			RConsole.println(x + " " + y);
+			
+			
+			if(i< listOfWayPoints.size()-1 && !listOfWayPoints.get(i).getAdjacencyList().contains(listOfWayPoints.get(i+1))){
+				ArrayList<Intersection> secondPartOfNewRoute = Dijkstra.algorithmForTargetZone(listOfWayPoints.get(i));
+				
+				for(int j=listOfWayPoints.size()-1; j>=i+1.;j--){
+					listOfWayPoints.remove(j);
+				}
+
+				for(int j=0; j < secondPartOfNewRoute.size();j++){
+					listOfWayPoints.add(secondPartOfNewRoute.get(j));
+				}
+			}
+			
 		}
+		
+		
 
 		/*
 		 * +--+--+--+--+ +--+--+--+--+ +--+--+--+--+ +--+--+--+--+ +--+--+--+--+
@@ -68,7 +104,8 @@ public class DijkstraTest {
 
 		for (int y = 0; y < Map.NUM_OF_INTERSECTIONS; y++) {
 			for (int x = 0; x < Map.NUM_OF_INTERSECTIONS + 1; x++) {
-				if ((k<listOfWayPoints.size()) && listOfWayPoints.get(k).getX() == (x - 1)
+				if ((k < listOfWayPoints.size())
+						&& listOfWayPoints.get(k).getX() == (x - 1)
 						&& listOfWayPoints.get(k).getY() == (y)) {
 					RConsole.print("x  ");
 					k++;
@@ -78,8 +115,8 @@ public class DijkstraTest {
 						&& Map.getIntersection((x - 1), y) == null) {
 					RConsole.print("#  ");
 				}
-				
-				else if((x > 0) && (y < Map.NUM_OF_INTERSECTIONS)
+
+				else if ((x > 0) && (y < Map.NUM_OF_INTERSECTIONS)
 						&& Map.getIntersection((x - 1), y).isTarget()) {
 					RConsole.print("$  ");
 				}

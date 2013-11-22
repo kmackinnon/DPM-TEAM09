a94516c4c8eb3dc2f662b4c5a0a5a72917405dbf
Keith MacKinnon <keith.mackinnon@mail.mcgill.ca>
2013-11-22 14:00:41 -0500

    Block acquisition test not working properly. It's not detecting the block until too late and won't perform the grabBlock() routine properly (it didn't move backwards).

--- src/testing/BlockMoverTest.java
+++ src/testing/BlockMoverTest.java
@@ -1,41 +1,54 @@
 package testing;
 
 import lejos.nxt.Button;
+import lejos.nxt.LCD;
 import lejos.nxt.Sound;
 import robot.BlockMover;
 import robot.MobileRobot;
 
+/**
+ * Tests the acquisition of a styrofoam block once one has been found.
+ * 
+ * @author Keith MacKinnon
+ * 
+ */
+
 public class BlockMoverTest {
 
 	public static void main(String[] args) {
-		
-		int option = 0;
-		
-		while (option == 0)
-			option = Button.waitForAnyPress();
 
-		
+		int buttonChoice = 0;
+
+		while (buttonChoice == 0)
+			buttonChoice = Button.waitForAnyPress();
+
 		BlockMover blockMover = new BlockMover();
-		
-		blockMover.liftClaw();
-		
+
+		blockMover.liftClaw(); // lift the claw at the beginning
+
 		MobileRobot.blockDetector.startBlockDetectorTimer();
 		MobileRobot.blockDetector.turnOnBlockDetection();
-		
-		
-		while(true){
-			if(MobileRobot.blockDetector.isObjectInFront()){
-				blockMover.stopMoving();
-				
-				if(MobileRobot.blockDetector.isObjectStyrofoam()){
-					blockMover.grabBlock();
-					System.exit(0);
-				}
-			}
+
+		boolean isSearching = true;
+		while (isSearching) {
 			
-			else{
+			// stop moving if there is something in front
+			if (MobileRobot.blockDetector.isObjectInFront()) {
+				blockMover.stopMoving();
+				isSearching = false; // i.e. it found a block
+
+				// if that object is styrofoam, perform grabBlock() method
+				if (MobileRobot.blockDetector.isObjectStyrofoam()) {
+					blockMover.grabBlock();
+					Sound.beep();
+				} else {
+					LCD.drawString("Not styro", 0, 0);
+				}
+				
+			} else {
 				blockMover.moveForward();
 			}
+
 		}
 
 	}
